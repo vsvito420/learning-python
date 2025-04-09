1c5302baf919aef21dd1ddc980b4183dd3c65220
VitoS <ikn0@proton.me>
2025-04-10 00:13:46 +0200

    Fix conf.py and add requirements.txt for Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -4,121 +4,49 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'learning-python'
 copyright = '2025, v.skolan'
 author = 'v.skolan'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import sys
+import os
 
-import recommonmark
-from recommonmark.transform import AutoStructify
-
-extensions = [
-    'recommonmark',
-]
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'learning-python'
-copyright = '2025, v.skolan'
-author = 'v.skolan'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'recommonmark',
-]
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes"]
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'learning-python'
-copyright = '2025, v.skolan'
-author = 'v.skolan'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# Add path to extensions
+sys.path.append(os.path.abspath('.'))
 
 extensions = [
     'recommonmark',
     'sphinx_rtd_theme',
 ]
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes"]
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# Add source file types
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
-project = 'learning-python'
-copyright = '2025, v.skolan'
-author = 'v.skolan'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'recommonmark',
-    'sphinx_rtd_theme',
-]
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes"]
-source_suffix = ['.rst', '.md']
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'learning-python'
-copyright = '2025, v.skolan'
-author = 'v.skolan'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = [
-    'recommonmark',
-    'sphinx_rtd_theme',
-]
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes"]
-source_suffix = ['.rst', '.md']
-
-def setup(app):
-    app.add_config_value('recommonmark_config', {
-            'enable_eval_rst': True,
-            'auto_toc_tree_depth': 2,
-        }, True)
-    app.add_transform(AutoStructify)
+# The master toctree document
+master_doc = 'index'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Language settings
 language = 'de'
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+def setup(app):
+    from recommonmark.transform import AutoStructify
+    app.add_config_value('recommonmark_config', {
+            'enable_eval_rst': True,
+            'auto_toc_tree_section': 'Contents',
+            'enable_math': True,
+            'enable_inline_math': True,
+            'enable_auto_doc_ref': True,
+        }, True)
+    app.add_transform(AutoStructify)
