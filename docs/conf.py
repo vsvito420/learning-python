def673fe44fb9e258da99336c53402d04ae2aa81
VitoS <ikn0@proton.me>
2025-04-10 00:10:07 +0200

    Enable Markdown support and fix file paths for Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,104 @@ author = 'v.skolan'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+import recommonmark
+from recommonmark.transform import AutoStructify
+
+extensions = [
+    'recommonmark',
+]
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'learning-python'
+copyright = '2025, v.skolan'
+author = 'v.skolan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'recommonmark',
+]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'learning-python'
+copyright = '2025, v.skolan'
+author = 'v.skolan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'recommonmark',
+    'sphinx_rtd_theme',
+]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'learning-python'
+copyright = '2025, v.skolan'
+author = 'v.skolan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'recommonmark',
+    'sphinx_rtd_theme',
+]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
+source_suffix = ['.rst', '.md']
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'learning-python'
+copyright = '2025, v.skolan'
+author = 'v.skolan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'recommonmark',
+    'sphinx_rtd_theme',
+]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes"]
+source_suffix = ['.rst', '.md']
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'enable_eval_rst': True,
+            'auto_toc_tree_depth': 2,
+        }, True)
+    app.add_transform(AutoStructify)
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/index.rst
+++ docs/index.rst
@@ -10,26 +10,26 @@ Willkommen bei learning-python's Dokumentation!
    :maxdepth: 2
    :caption: Inhaltsverzeichnis:
 
-   Anleitungen/00_anleitung
-   Anleitungen/01_Kommentare
-   Anleitungen/02_Algorithmen
-   Anleitungen/03_python_interpreter
-   Anleitungen/04_variablen_und_Datentypen
-   Anleitungen/05_string_operationen
-   Anleitungen/06_lernweg
-   Anleitungen/07_bedingungen
+   Anleitungen/00_anleitung.md
+   Anleitungen/01_Kommentare.md
+   Anleitungen/02_Algorithmen.md
+   Anleitungen/03_python_interpreter.md
+   Anleitungen/04_variablen_und_Datentypen.md
+   Anleitungen/05_string_operationen.md
+   Anleitungen/06_lernweg.md
+   Anleitungen/07_bedingungen.md
 
-   Aufgaben/Kapitel_0/Anfang_Lese_Mich
-   Aufgaben/Kapitel_1/Grundlagen
-   Aufgaben/Kapitel_1/Strings
-   Aufgaben/Kapitel_2/Bedingungen_und_Dictionaries
-   Aufgaben/Kapitel_3/Turtle_Grafik
-   Aufgaben/Kapitel_4/Spiele
-   Aufgaben/Kapitel_5/Kompliment_Generator
-   Aufgaben/Kapitel_6/Funktionen
-   Aufgaben/Kapitel_7/Module_und_Builtin_Funktionen
-   Aufgaben/Kapitel_8/Funktionsdesign
-   Aufgaben/Kapitel_9/Pygame_Ghost_Game
+   Aufgaben/Kapitel_0/Anfang_Lese_Mich.md
+   Aufgaben/Kapitel_1/Grundlagen.md
+   Aufgaben/Kapitel_1/Strings.md
+   Aufgaben/Kapitel_2/Bedingungen_und_Dictionaries.md
+   Aufgaben/Kapitel_3/Turtle_Grafik.md
+   Aufgaben/Kapitel_4/Spiele.md
+   Aufgaben/Kapitel_5/Kompliment_Generator.md
+   Aufgaben/Kapitel_6/Funktionen.md
+   Aufgaben/Kapitel_7/Module_und_Builtin_Funktionen.md
+   Aufgaben/Kapitel_8/Funktionsdesign.md
+   Aufgaben/Kapitel_9/Pygame_Ghost_Game.md
 
 Indices and tables
 ==================
